92d2ec89f586eb89282ac3e6fe14ca7cb9ca6173
Giuseppe Ambrosio <gi.ambrosio@reply.it>
2022-03-23 15:47:58 +0100

    update setup.py

--- heuristic_algorithm/setup.py
+++ heuristic_algorithm/setup.py
@@ -1,11 +1,27 @@
 from setuptools import setup, find_packages
-# List of requirements
-requirements = []  # This could be retrieved from requirements.txt
-# Package (minimal) configuration
-setup(
-    name="src",
-    version="1.0.0",
-    description="Master's thesis",
-    packages=find_packages(),  # __init__.py folders search
-    install_requires=requirements
-)
+import os
+
+
+# Function to retrieve resources files
+def _get_resources(package_name):
+    # Get all the resources (also on nested levels)
+    res_paths = os.path.join(package_name, "resources")
+    all_resources = [os.path.join(folder, file) for folder, _, files in os.walk(res_paths) for file in files]
+    # Remove the prefix: start just from "resources"
+    return [resource[resource.index("resources"):] for resource in all_resources]
+
+
+# Read requirements
+requirements_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
+with open(requirements_file, "r") as f:
+    requirements = f.read().splitlines()
+
+pkg_name = "heuristic_algorithm"
+
+# Package configuration
+setup(name=pkg_name,
+      version="0.0.0",
+      description="Clustering Deletion Heuristic Algorithm",
+      packages=find_packages(),
+      package_data={pkg_name: _get_resources(pkg_name), "tests": ["stubs/*"]},
+      install_requires=requirements)
